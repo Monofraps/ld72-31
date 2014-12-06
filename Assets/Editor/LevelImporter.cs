212350ae331c0b6ecbd25c09a1d033e33e6c747e
Monofraps <monofraps@gmail.com>
2014-12-07 00:06:20 +0100

    Implemented LevelImporter editor action Changed LevelGenerator to work with importer editor action

---
+++ Assets/Editor/LevelImporter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Collections;
+
+class LevelImporterWindow : EditorWindow
+{
+    public string levelText;
+    public GameObject wallPrefab;
+    public GameObject coloredWallPrefab;
+    public GameObject colorizerPrefab;
+    public GameObject levelRoot;
+
+    [MenuItem("LD31/Import Level")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(LevelImporterWindow));
+    }
+
+    private LevelGenerator levelgen;
+
+    void OnGUI()
+    {
+        levelText = EditorGUILayout.TextArea(levelText, GUILayout.Height(200));
+
+        if (GUILayout.Button("Import"))
+        {
+         levelgen = new LevelGenerator(levelText, wallPrefab, coloredWallPrefab, colorizerPrefab);  
+            levelgen.readLevel();
+        }
+
+        if (GUILayout.Button("Set Wall Prefab"))
+        {
+            wallPrefab = (GameObject) PrefabUtility.GetPrefabParent(Selection.activeObject);
+        }
+
+        if (GUILayout.Button("Set Colored Wall Prefab"))
+        {
+            coloredWallPrefab = (GameObject)PrefabUtility.GetPrefabParent(Selection.activeObject);
+        }
+
+
+        if (GUILayout.Button("Set Colorizer Prefab"))
+        {
+            colorizerPrefab = (GameObject)PrefabUtility.GetPrefabParent(Selection.activeObject);
+        }
+
+        if (GUILayout.Button("Set Level Root"))
+        {
+            levelRoot = Selection.activeGameObject;
+        }
+    }
+}
--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -1,91 +1,86 @@
-﻿ï»¿using UnityEngine;
+﻿using UnityEngine;
 using System.Collections.Generic;
 using System.Collections;
 using System;
 
-public class LevelGenerator  {
-	public string levelName;
-	private List<List<char>> list = new List<List<char>> ();
-	private List<char> listLine;
-	public GameObject wallTile;
-	public GameObject wallTileCollered;
-	public GameObject colorizeField;
-	public Color redColor;
-	public Color greenColor;
-	public Color blueColor;
+public class LevelGenerator
+{
+    public string levelName;
+    private List<List<char>> list = new List<List<char>>();
+    private List<char> listLine;
+    public GameObject wallTile;
+    public GameObject wallTileCollered;
+    public GameObject colorizeField;
+
+    public LevelGenerator(string levelName, GameObject wallTile, GameObject wallTileCollered, GameObject colorizeField)
+    {
+        this.levelName = levelName;
+        this.wallTile = wallTile;
+        this.wallTileCollered = wallTileCollered;
+        this.colorizeField = colorizeField;
+    }
+
+    public void readLevel()
+    {
+        List<string> lines = new List<string>(levelName.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries));
+        Debug.Log(lines.Count);
+        foreach (string line in lines)
+        {
+            listLine = new List<char>(line.ToCharArray());
+            list.Add(listLine);
+        }
+        int countX = 0;
+        int countY = 0;
+        GameObject forCollering;
+
+        foreach (List<char> listLine in list)
+        {
+            foreach (char sign in listLine)
+            {
+
+
+                switch (sign)
+                {
+                    case 'R':
+                        forCollering = ((GameObject)GameObject.Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
+                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Red;
+                        break;
+
+                    case 'G':
+                        forCollering = ((GameObject)GameObject.Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
+                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Green;
+                        break;
+
+                    case 'B':
+                        forCollering = ((GameObject)GameObject.Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
+                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Blue;
+                        break;
+
+                    case 'W':
+                        GameObject.Instantiate(wallTile, new Vector3(countX, -countY, 0), Quaternion.identity);
+                        break;
+
+                    case 'r':
+                        forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
+                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Red;
+                        break;
+                    case 'g':
+                        forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
+                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Green;
+                        break;
+                    case 'b':
+                        forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
+                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Blue;
+                        break;
+
+                }
+                countX++;
+            }
+            countY++;
+            countX = 0;
+        }
 
 
 
-
-	// Use this for initialization
-	void Start (string levelName, GameObject wallTile, GameObject wallTileCollered)
-	{
-	    this.levelName = levelName;
-	    this.wallTile = wallTile;
-	    this.wallTileCollered = wallTileCollered;
-	}
-
-	public void readLevel(){
-		List<string> lines = new List<string>(levelName.Split (new string[] {Environment.NewLine}, StringSplitOptions.RemoveEmptyEntries));
-		Debug.Log (lines.Count);
-		foreach(string line in lines){	
-			listLine = new List<char> (line.ToCharArray ());
-			list.Add (listLine);
-		}
-		int countX = 0;
-		int countY = 0;
-		GameObject forCollering;
-
-		foreach(List<char> listLine in list){
-			foreach(char sign in listLine){
-
-
-				switch (sign) 
-				{
-					case 'R':
-					forCollering = ((GameObject)Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
-					forCollering.GetComponent<ColorizerField>().fieldColor= ColorizationColors.Red;
-					break;
-
-					case 'G':
-					forCollering = ((GameObject)Instantiate(wallTileCollered, new Vector3(countX,-countY, 0), Quaternion.identity));
-					forCollering.GetComponent<ColorizerField>().fieldColor= ColorizationColors.Green;
-					break;
-
-					case 'B':
-					forCollering = ((GameObject)Instantiate(wallTileCollered, new Vector3(countX,-countY, 0), Quaternion.identity));
-					forCollering.GetComponent<ColorizerField>().fieldColor= ColorizationColors.Blue;
-					break;
-
-					case 'W':
-					Instantiate(wallTile, new Vector3(countX, -countY, 0), Quaternion.identity);
-					break;
-
-				case 'r':
-					forCollering = ((GameObject)Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
-					forCollering.GetComponent<ColorizerField>().fieldColor= ColorizationColors.Red;
-						break;
-				case 'g':
-					forCollering = ((GameObject)Instantiate(colorizeField, new Vector3(countX,-countY, 0), Quaternion.identity));
-					forCollering.GetComponent<ColorizerField>().fieldColor= ColorizationColors.Green;
-						break;
-				case 'b':
-					forCollering = ((GameObject)Instantiate(colorizeField, new Vector3(countX,-countY, 0), Quaternion.identity));
-					forCollering.GetComponent<ColorizerField>().fieldColor= ColorizationColors.Blue;
-						break;
-
-				}
-				countX++;
-			}
-			countY++;
-			countX = 0;
-		}
-
-
-
-		}
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    }
 }
