671fe4eb25d13abd4b85c1ecadd946467dfd6080
Monofraps <monofraps@gmail.com>
2014-12-06 12:54:54 +0100

    Added demo files for pickups

---
+++ Assets/Testing/Scripts/PickupField.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupField : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PowerupController powerupController = collision.gameObject.GetComponent<PowerupController>();
+            powerupController.Callback();
+
+            GameObject.Destroy(gameObject);
+        }
+
+    }
+}
