f5b7c4b1cfee962184b997a6643c7c566af66fa4
Monofraps <monofraps@gmail.com>
2014-12-06 13:39:31 +0100

    Added item related classes

---
+++ Assets/Scripts/Items/ItemRegistry.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class ItemRegistry : MonoBehaviour
+{
+    public static ItemRegistry Instance { get; private set; }
+
+    public List<GameObject> itemPrefabs;
+
+    void Start()
+    {
+        Instance = this;
+    }
+
+    public void InstantiateRandomItem()
+    {
+        
+    }
+}
