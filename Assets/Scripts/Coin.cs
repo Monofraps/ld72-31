aa723b4b0e0b6c2224150d95e1a72f7c5afa425c
unknown <D062347@WDFN00351625A.emea.global.corp.sap>
2014-12-06 14:16:01 +0100

    Added Coin coonntroller

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			CoinController coinController = collision.gameObject.GetComponent<CoinController>();
+			coinController.collectChoin();
+			
+			GameObject.Destroy(gameObject);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinController : MonoBehaviour {
+	private int choinsCount = 0;
+	public CoinCounterText choinText;
+
+	public void collectChoin(){
+		choinsCount++;
+		choinText.setCointCounter (choinsCount);
+		}
+
+}
