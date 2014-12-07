7b392e0b81d8f32d395c0c73010bdb02273ad20f
Monofraps <monofraps@gmail.com>
2014-12-07 01:56:32 +0100

    Updated GameManager Created example level prefab

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -1,34 +1,38 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public static GameController Instance { get; private set; }
 
-    public List<LevelStateController> levelRoots;
-    private int currentLevel = 0;
+    public List<GameObject> levelPrefabs;
+
+    private LevelStateController currentLevel;
+    private int currentLevelIndex = 0;
 
     private void Start()
     {
         Instance = this;
-
-        foreach (var levelRoot in levelRoots)
-        {
-            levelRoot.gameObject.SetActive(false);
-        }
-
-        levelRoots[currentLevel].gameObject.SetActive(true);
+        LoadNextLevel();
     }
 
     public void PublishPlayerColorChangeEvent(ColorizationColors newPlayerColor)
     {
-        levelRoots[currentLevel].PublishPlayerColorChangeEvent(newPlayerColor);
+        currentLevel.PublishPlayerColorChangeEvent(newPlayerColor);
+    }
+
+    private void LoadNextLevel()
+    {
+        currentLevel = ((GameObject)Instantiate(levelPrefabs[currentLevelIndex])).GetComponent<LevelStateController>();
     }
 
     public void ProgressLevel()
     {
-        levelRoots[currentLevel].gameObject.SetActive(false);
-        levelRoots[++currentLevel].gameObject.SetActive(true);
+        currentLevel.gameObject.SetActive(false);
+        Destroy(Camera.current.gameObject);
+        ++currentLevelIndex;
+        LoadNextLevel();
     }
 }
