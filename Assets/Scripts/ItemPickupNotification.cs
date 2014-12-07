30ac29f683270e21e3d6e4b21898cf2521a1252a
Monofraps <monofraps@gmail.com>
2014-12-07 10:41:56 +0100

    Added UI animations Updated build settings

--- Assets/Scripts/ItemPickupNotification.cs
+++ Assets/Scripts/ItemPickupNotification.cs
@@ -7,45 +7,97 @@ public class ItemPickupNotification : MonoBehaviour
     public Text text;
     public Image itemImage;
 
-    public double showDuration = 2;
+    public double fadeDuration = .1f;
 
-    private double elapsedTime = 0;
-    private bool isDisplaying = false;
+    public double fadeTime = 0;
+
+    public Vector3 imageStartPosition;
+    public Vector3 imageEndPosition;
+
+    public Vector3 textStartPosition;
+    public Vector3 textEndPosition;
+
+    public AnimationCurve animationCurve;
+
+    private enum NotificationFadeState
+    {
+        FadeIn, FadeOut, Visible, Hidden
+    }
+
+    private NotificationFadeState fadeState = NotificationFadeState.Hidden;
 
     void Start()
     {
+        text.gameObject.SetActive(false);
         text.text = "";
         itemImage.gameObject.SetActive(false);
     }
 
     public void ShowPickupNotification(ItemBase item)
     {
+        text.gameObject.SetActive(true);
         text.text = item.GetDisplayName();
         itemImage.gameObject.SetActive(true);
         itemImage.sprite = item.GetSprite();
-        isDisplaying = true;
+        fadeState = NotificationFadeState.FadeIn;
     }
 
     public void ClosePickupNotification()
     {
+        fadeState = NotificationFadeState.FadeOut;
+    }
+
+    public void HidePickupNotification()
+    {
+        text.gameObject.SetActive(false);
         text.text = "";
         itemImage.gameObject.SetActive(false);
-        elapsedTime = 0;
-        isDisplaying = false;
+        fadeState = NotificationFadeState.Hidden;
     }
-    /*
+
     void Update()
     {
-        if (!isDisplaying)
+        switch (fadeState)
         {
-            return; 
+            case NotificationFadeState.FadeIn:
+                fadeTime += Time.deltaTime;
+                UpdatePositions();
+                if (ShouldChangeState())
+                {
+                    fadeState = NotificationFadeState.Visible;
+                    fadeTime = fadeDuration;
+                }
+                break;
+
+            case NotificationFadeState.FadeOut:
+                fadeTime -= Time.deltaTime;
+                UpdatePositions();
+                if (ShouldChangeState())
+                {
+                    fadeState = NotificationFadeState.Hidden;
+                    fadeTime = 0;
+                }
+                break;
         }
 
-        elapsedTime += Time.deltaTime;
+    }
 
-        if (elapsedTime >= showDuration)
+    private void UpdatePositions()
+    {
+        if (IsInTransitionState())
         {
-            HidePickupNotification();
+            itemImage.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(imageStartPosition, imageEndPosition, animationCurve.Evaluate((float)(fadeTime / fadeDuration)));
+            text.GetComponent<RectTransform>().anchoredPosition = Vector3.Lerp(textStartPosition, textEndPosition, animationCurve.Evaluate((float)(fadeTime / fadeDuration)));
         }
-    }*/
+    }
+
+    private bool IsInTransitionState()
+    {
+        return fadeState == NotificationFadeState.FadeIn || fadeState == NotificationFadeState.FadeOut;
+    }
+
+    private bool ShouldChangeState()
+    {
+        return fadeTime >= fadeDuration || fadeTime <= 0;
+    }
 }
