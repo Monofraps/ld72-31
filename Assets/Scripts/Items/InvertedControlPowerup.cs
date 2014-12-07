ab8368c0be546d1769b594da1afa66ef663a73bd
Monofraps <monofraps@gmail.com>
2014-12-07 15:41:07 +0100

    Added inverted control powerup

---
+++ Assets/Scripts/Items/InvertedControlPowerup.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InvertedControlPowerup : ItemBase {
+
+    public float duration;
+    private PlayerControler playerControler;
+
+    public override void Activate()
+    {
+        playerControler = PlayerControler.FindInScene();
+        playerControler.IsControlInverted = true;
+        Invoke("DisableControlInversion", duration);
+    }
+
+    private void DisableControlInversion()
+    {
+        playerControler.IsControlInverted = false;
+        Destroy(gameObject);
+    }
+}
