06ac31f8b43cb2585ab9d0c63afb0354a95d5be7
Sisa241995 <charlotte.proeller@gmx.de>
2014-12-07 02:36:52 +0100

    implement I for pickups in LevelGenerator first Level is loaded, without start,end,enemys, enemy paths, spezifiktion of Items, C(h)oins

--- Assets/Scripts/LevelGenerator.cs
+++ Assets/Scripts/LevelGenerator.cs
@@ -11,13 +11,17 @@ public class LevelGenerator
     public GameObject wallTile;
     public GameObject wallTileCollered;
     public GameObject colorizeField;
+	public GameObject pickUpItem;
+	public GameObject parent;
 
-    public LevelGenerator(string levelName, GameObject wallTile, GameObject wallTileCollered, GameObject colorizeField)
+    public LevelGenerator(string levelName, GameObject wallTile, GameObject wallTileCollered, GameObject colorizeField, GameObject pickUpItem, GameObject parent)
     {
         this.levelName = levelName;
         this.wallTile = wallTile;
         this.wallTileCollered = wallTileCollered;
         this.colorizeField = colorizeField;
+		this.pickUpItem = pickUpItem;
+		this.parent = parent;
     }
 
     public void readLevel()
@@ -43,35 +47,47 @@ public class LevelGenerator
                 {
                     case 'R':
                         forCollering = ((GameObject)GameObject.Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
-                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Red;
+					forCollering.GetComponent<ColoredWallTile>().tileColor = ColorizationColors.Red;
+					forCollering.transform.parent = parent.transform;
                         break;
 
                     case 'G':
                         forCollering = ((GameObject)GameObject.Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
-                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Green;
+					Debug.Log(forCollering);
+					forCollering.GetComponent<ColoredWallTile>().tileColor = ColorizationColors.Green;
+					forCollering.transform.parent = parent.transform;
                         break;
 
                     case 'B':
                         forCollering = ((GameObject)GameObject.Instantiate(wallTileCollered, new Vector3(countX, -countY, 0), Quaternion.identity));
-                        forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Blue;
+					forCollering.GetComponent<ColoredWallTile>().tileColor = ColorizationColors.Blue;
+					forCollering.transform.parent = parent.transform;
                         break;
 
                     case 'W':
-                        GameObject.Instantiate(wallTile, new Vector3(countX, -countY, 0), Quaternion.identity);
+					forCollering = (GameObject)GameObject.Instantiate(wallTile, new Vector3(countX, -countY, 0), Quaternion.identity);
+					forCollering.transform.parent = parent.transform;
                         break;
 
                     case 'r':
                         forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
                         forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Red;
+					forCollering.transform.parent = parent.transform;
                         break;
                     case 'g':
                         forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
                         forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Green;
+					forCollering.transform.parent = parent.transform;
                         break;
                     case 'b':
-                        forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
+					forCollering = ((GameObject)GameObject.Instantiate(colorizeField, new Vector3(countX, -countY, 0), Quaternion.identity));
                         forCollering.GetComponent<ColorizerField>().fieldColor = ColorizationColors.Blue;
+					forCollering.transform.parent = parent.transform;
                         break;
+				case 'I':
+					forCollering = (GameObject)GameObject.Instantiate(pickUpItem, new Vector3(countX, -countY, 0), Quaternion.identity);
+					forCollering.transform.parent = parent.transform;
+					break;
 
                 }
                 countX++;
